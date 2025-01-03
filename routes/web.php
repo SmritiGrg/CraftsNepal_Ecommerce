60a53bd8608d2c7c5f607f89d725b356c585ebe1
Smriti Grg <gurungsm.10@gmail.com>
2025-01-03 14:40:19 +0545

    product category controller route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\FileController;
+use App\Http\Controllers\ProductCategoryController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,7 +18,7 @@ Route::middleware('auth')->group(function () {
     // Route::get('/', function () {
     //     return view('admin.index');
     // })->name('admin.index');
-    Route::resource('/file', FileController::class);
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -25,6 +26,8 @@ Route::middleware('auth')->group(function () {
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+    Route::resource('/file', FileController::class);
+    Route::resource('/productCategort', ProductCategoryController::class);
 });
 
 require __DIR__ . '/auth.php';
