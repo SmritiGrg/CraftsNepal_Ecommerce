4673c632e32a091f73e47dbacf4553a5f56e06c5
bipana chapain <chapai.bipana65@gmail.com>
2025-01-24 13:09:40 +0545

    modified controller name

--- app/Http/Controllers/customerOrderController.php
+++ app/Http/Controllers/customerOrderController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Order;
 use Illuminate\Http\Request;
 
-class cutomerOrderController extends Controller
+class customerOrderController extends Controller
 {
     public function index()
     {
--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,18 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\adminOrderController;
 use App\Http\Controllers\cartcontroller;
 use App\Http\Controllers\customerProductController;
-use App\Http\Controllers\cutomerOrderController;
+use App\Http\Controllers\customerOrderController;
 use App\Http\Controllers\FileController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductCategoryController;
-use App\Http\Controllers\productController;
+use App\Http\Controllers\AdminProductController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\UserProductController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('CraftsNepal/index');
 // });
-Route::resource('/',UserProductController::class);
+Route::resource('/',HomeController::class);
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -37,14 +37,14 @@ Route::delete('/cart/remove/{id}', [cartController::class, 'remove'])->name('car
 Route::get('/checkout', [cartController::class, 'checkout'])->name('cart.checkout');
 Route::post('/order/place', [cartController::class, 'placeOrder'])->name('order.place');
 Route::resource('userproduct',customerProductController::class);
-Route::resource('userorder',cutomerOrderController::class);
+Route::resource('userorder',customerOrderController::class);
 });
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
     Route::resource('admin/file', FileController::class);
     Route::resource('/productCategory', ProductCategoryController::class);
-    Route::resource('product',productController::class);
+    Route::resource('product',AdminProductController::class);
     Route::resource('admin/order',adminOrderController::class);
 });
 
